8c322987bcb81b3150c3c6e71a1ad94b57c078de
codez0mb1e <d.petukhov@outlook.com>
2023-03-23 17:20:34 +0000

    Fix imports

--- cryptpandas/__init__.py
+++ cryptpandas/__init__.py
@@ -1,8 +1,12 @@
 """
 @author: Luca Mingarelli
 """
+from cryptpandas.key_vaults import AzureKeyVaultManager, KeyVaultException
+from cryptpandas.protect import DataframeProtector, ProtectorSettings
+
+
 __about__ = "A tool for encryption and decryption of pandas dataframes."
-__version__= '1.1.0'
+__version__= '1.1.1'
 __author__ = 'Luca Mingarelli'
 __email__ = "lucamingarelli@me.com"
 __url__ = "https://github.com/LucaMingarelli/cryptpandas"
