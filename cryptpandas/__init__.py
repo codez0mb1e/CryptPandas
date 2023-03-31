35dea13c65cc30ba356d7e507371123e30d20fdb
codez0mb1e <d.petukhov@outlook.com>
2023-03-31 16:17:14 +0000

    Improve setup.py

--- cryptpandas/__init__.py
+++ cryptpandas/__init__.py
@@ -6,7 +6,7 @@ from cryptpandas.protect import DataframeProtector, ProtectorSettings
 
 
 __about__ = "A tool for encryption and decryption of pandas dataframes."
-__version__= '1.1.1'
+__version__= '1.1.2'
 __author__ = 'Luca Mingarelli'
 __email__ = "lucamingarelli@me.com"
 __url__ = "https://github.com/LucaMingarelli/cryptpandas"
--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-import setuptools
 import cryptpandas as crp
+from setuptools import find_packages, setup
 
-with open("README.md", 'r') as f:
+
+with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("requirements.txt", "r") as f:
+    requirements = [x for x in f.read().splitlines() if "#" not in x]
 
-setuptools.setup(
+
+setup(
     name="CryptPandas",
     version=crp.__version__,
     author=crp.__author__,
     author_email=crp.__email__,
     description=crp.__about__,
     url=crp.__url__,
-    license='MIT',
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    install_requires=['pandas', 'cryptography','pyarrow'],
+    packages=find_packages(exclude="tests"),
+    install_requires=requirements,
     classifiers=["Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License",
                  "Operating System :: OS Independent"],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
